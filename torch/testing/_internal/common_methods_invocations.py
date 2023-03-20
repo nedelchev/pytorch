e4e184c46d1df675fdd47af39a78cfcc26d97d00
BJ Hargrave <hargrave@us.ibm.com>
2023-03-20 09:35:31 -0400

    Enable bitwise shift operations tests
    With #70904 fixed, we can remove the skips for the bitwise shift tests.
    
    Signed-off-by: BJ Hargrave <hargrave@us.ibm.com>

--- torch/testing/_internal/common_methods_invocations.py
+++ torch/testing/_internal/common_methods_invocations.py
@@ -9774,8 +9774,6 @@ op_db: List[OpInfo] = [
                     rhs_make_tensor_kwargs=dict(low=0),
                     skips=(
                         DecorateInfo(unittest.skip("Skipped!"), 'TestBinaryUfuncs', 'test_type_promotion'),
-                        # https://github.com/pytorch/pytorch/issues/70904
-                        DecorateInfo(unittest.skip("Some inputs produce undefined outputs"), 'TestCommon', 'test_compare_cpu'),
                     )),
     BinaryUfuncInfo('bitwise_right_shift',
                     op=torch.bitwise_right_shift,
@@ -9788,8 +9786,6 @@ op_db: List[OpInfo] = [
                     rhs_make_tensor_kwargs=dict(low=0),
                     skips=(
                         DecorateInfo(unittest.skip("Skipped!"), 'TestBinaryUfuncs', 'test_type_promotion'),
-                        # https://github.com/pytorch/pytorch/issues/70904
-                        DecorateInfo(unittest.skip("Some inputs produce undefined outputs"), 'TestCommon', 'test_compare_cpu'),
                     )),
     OpInfo('combinations',
            op=torch.combinations,
@@ -18979,19 +18975,11 @@ python_ref_db = [
         "_refs.bitwise_left_shift",
         torch_opinfo_name="bitwise_left_shift",
         supports_nvfuser=False,
-        skips=(
-            # https://github.com/pytorch/pytorch/issues/70904
-            DecorateInfo(unittest.skip("Some inputs produce undefined outputs"), 'TestCommon', 'test_compare_cpu'),
-        ),
     ),
     ElementwiseBinaryPythonRefInfo(
         "_refs.bitwise_right_shift",
         torch_opinfo_name="bitwise_right_shift",
         supports_nvfuser=False,
-        skips=(
-            # # https://github.com/pytorch/pytorch/issues/70904
-            DecorateInfo(unittest.skip("Skipped some inputs produce undefined outputs"), 'TestCommon', 'test_compare_cpu'),
-        ),
     ),
     ElementwiseBinaryPythonRefInfo(
         "_refs.bitwise_or",
