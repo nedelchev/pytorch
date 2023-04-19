29d66ce406e1d6416139fb697c99124b2e4cebc2
Pearu Peterson <pearu.peterson@gmail.com>
2023-04-19 23:19:22 +0300

    Fix incorrect sparse_dim in COO.zero_() and in binary operations with zero-sized COO operands
    ghstack-source-id: 32e32b1c367e1e9cd3f0fc9ac49e792565cdcc90
    Pull Request resolved: https://github.com/pytorch/pytorch/pull/98292

--- torch/testing/_internal/opinfo/definitions/sparse.py
+++ torch/testing/_internal/opinfo/definitions/sparse.py
@@ -137,14 +137,6 @@ def _sample_and_error_inputs_mul_sparse(
                 sample,
                 error_regex="crow_indices is supposed to be a vector, but got 2 dimensional tensor",
             )
-        elif layout is torch.sparse_csr and t_inp.numel() == 0 and t_args[0].ndim > 0:
-            yield ErrorInput(
-                sample,
-                error_regex=(
-                    "Only tensors with two sparse dimensions can be converted to the SparseCsr layout,"
-                    " got self with 3 sparse dimensions"
-                ),
-            )
         elif layout is torch.sparse_csc and t_args[0].ndim > 0:
             yield ErrorInput(
                 sample, error_regex="Expected result Tensor to be of format CSR"
